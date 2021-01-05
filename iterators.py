810e97dbcdc96582fe2baa0163a49bac7d87119a
Monish <66905892+monish-mnjds@users.noreply.github.com>
2021-01-05 22:26:02 +0530

    Update iterators.py

--- iterators.py
+++ iterators.py
@@ -24,3 +24,71 @@ next(my_iter)
 # This will raise error, no items left
 ---------------------------------------------
 
+#elegant way of iterating
+
+for element in my_list:
+  print(element)
+  
+---------------------------------------------
+
+class PowTwo:
+    """Class to implement an iterator
+    of powers of two"""
+
+    def __init__(self, max=0):
+        self.max = max
+
+    def __iter__(self):
+        self.n = 0
+        return self
+
+    def __next__(self):
+        if self.n <= self.max:
+            result = 2 ** self.n
+            self.n += 1
+            return result
+        else:
+            raise StopIteration
+
+
+# create an object
+numbers = PowTwo(3)
+
+# create an iterable from the object
+i = iter(numbers)
+
+# Using next to get to the next iterator element
+print(next(i))
+print(next(i))
+print(next(i))
+print(next(i))
+print(next(i))
+
+# We can also use a for loop to iterate over our iterator class.
+
+for i in PowTwo(5):
+  print(i)
+
+-----------------------------------------------------
+
+#infinite iterator
+
+class InfIter:
+    """Infinite iterator to return all
+        odd numbers"""
+
+    def __iter__(self):
+        self.num = 1
+        return self
+
+    def __next__(self):
+        num = self.num
+        self.num += 2
+        return num
+           
+a = iter(InfIter())
+next(a)
+next(a)
+next(a)        #this will keep on returning odd numbers.....
+
+---------------------------------------------------------------------

2f20cf7833df9e2b9cc4e56a237c2d057b424bc4
Monish <66905892+monish-mnjds@users.noreply.github.com>
2021-01-05 22:07:28 +0530

    Update iterators.py

--- iterators.py
+++ iterators.py
@@ -1,1 +1,26 @@
+my_list = [4, 7, 0, 3]
+# define a list 
+
+my_iter = iter(my_list)
+# get an iterator using iter()
+
+# iterate through it using next()
+
+print(next(my_iter))
+# Output: 4
+
+print(next(my_iter))
+# Output: 7
+
+# next(my_iter) is same as my_iter.__next__()
+
+print(my_iter.__next__())
+# Output: 0
+
+print(my_iter.__next__())
+# Output: 3
+
+next(my_iter)
+# This will raise error, no items left
+---------------------------------------------
 

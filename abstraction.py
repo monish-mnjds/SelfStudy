2041217417b00be72b9a9846fda58a368f3a90d6
Monish <66905892+monish-mnjds@users.noreply.github.com>
2021-01-16 22:34:28 +0530

    Update abstraction.py

--- abstraction.py
+++ abstraction.py
@@ -4,17 +4,17 @@ class Shape(ABC):     #abstract class
         pass
 
 class Rectangle(Shape):
-  length = 5
-  breadth = 3
-  def calculate_area(self):
-      return self.length * self.breadth
+    length = 5
+    breadth = 3
+    def calculate_area(self):
+        return self.length * self.breadth
 
 class Circle(Shape):
-  radius = 4
-  def calculate_area(self):
-      return 3.14 * self.radius * self.radius
+    radius = 4
+    def calculate_area(self):
+        return 3.14 * self.radius * self.radius
 
-rec =Rectangle()    #object created for the class 'Rectangle'
+rec = Rectangle()    #object created for the class 'Rectangle'
 cir = Circle()    #object created for the class 'Circle'
 print("Area of a rectangle:", rec.calculate_area())     #call to 'calculate_area' method defined inside the class 'Rectangle'
 print("Area of a circle:", cir.calculate_area())    #call to 'calculate_area' method defined inside the class 'Circle'

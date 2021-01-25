4a88c05b52789d93ea1f519052e309d4ef6a12e0
Monish <66905892+monish-mnjds@users.noreply.github.com>
2021-01-25 11:21:58 +0530

    Update HackerRank.py

--- HackerRank.py
+++ HackerRank.py
@@ -92,3 +92,21 @@ if __name__ == '__main__':
     result = wrap(string, max_width)
     print(result)
 --------------------------------------------------------------------------------
+6) '''If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird'''
+
+n = int(input('n: '))
+
+if n % 2 == 0:
+    if (n >= 2 and n <= 5):
+        print('Not Weird')
+    elif (n >= 6 and n <= 20):
+        print('Weird')
+    elif (n > 20):  
+        print('Not Weird')
+
+if n % 2 == 1:
+    print('Weird')
+---------------------------------------------------------------------------------

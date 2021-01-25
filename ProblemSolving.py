d9724593e96562c8bc09717af77cd5a0f74178f2
Monish <66905892+monish-mnjds@users.noreply.github.com>
2021-01-25 14:08:23 +0530

    Create ProblemSolving.py

---
+++ ProblemSolving.py
@@ -0,0 +1,12 @@
+#staircase
+
+def staircase(n):
+    for i in range(1, n+1):
+        s = '#' * i
+        print(s.rjust(n))
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
+    
+-----------------------------------

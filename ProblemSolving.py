795165a662ba337491b9805171f88697aa05fff7
Monish <66905892+monish-mnjds@users.noreply.github.com>
2021-01-25 15:37:31 +0530

    Update ProblemSolving.py

--- ProblemSolving.py
+++ ProblemSolving.py
@@ -19,3 +19,16 @@ if __name__ == '__main__':
     print([ [a, b, c] for a in range(0, x+1) for b in range(0, y+1) for c in range(0, z+1) if a+b+c != n])
     
 --------------------------------------------------------------------------------------------------------------
+#runner-up
+n = int(input())
+arr = map(int, input().split())
+
+a = max(arr)
+
+c = arr.count(a)
+
+for i in range(c):
+    arr.remove(a)
+
+print(max(arr))
+--------------------------------------------

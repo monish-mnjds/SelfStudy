9b001b691229ccc46ad4f4baf23cef554b2e6148
Monish <66905892+monish-mnjds@users.noreply.github.com>
2021-03-14 15:03:12 +0530

    Update InstanceClassStatic.py

--- InstanceClassStatic.py
+++ InstanceClassStatic.py
@@ -1,27 +1,27 @@
 class Sony:
 
-    division = 'Software Architecture Division'
+    __division = 'Software Architecture Division'
 
     def __init__(self, fullname):
-        self.ename = fullname
+        self.fullname = fullname
 
     def email(self):
-        self.first, self.last = self.ename.split(' ')
+        self.first, self.last = self.fullname.split(' ')
         return self.first + '.' + self.last + '@sony.com'
 
     @classmethod
     def change_dept(cls, division):
-        cls.division = division
+        cls.__division = division
 
     @staticmethod
     def new_dept():
-        return Sony.division
+        return Sony.__division
 
 s1 = Sony('Monish Kalaiselvan')
 s2 = Sony('Sagar Ramesh')
 
-print(Sony.division)
-
+#print(Sony._Sony__division)   #Private class variable can be accessed... 
+                               #But not recommended
 Sony.change_dept('SARD')
 
 print(s1.new_dept())

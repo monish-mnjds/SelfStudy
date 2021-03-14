9bcf88e582633703d3ea9110806a18dd04b97ef5
Monish <66905892+monish-mnjds@users.noreply.github.com>
2021-03-14 17:11:02 +0530

    Update Property.py

--- Property.py
+++ Property.py
@@ -25,3 +25,31 @@ del s.team
 s.team = 'SWQA'
 
 ----------------------------------------
+# property()
+------------
+
+class Sony:
+    def __init__(self, project):
+        self._project = project
+
+    def getTeam(self):
+        return self._project
+
+    def setTeam(self, project):
+        self._project = project
+
+    def delTeam(self):
+        del self._project
+
+    project = property(getTeam, setTeam, delTeam)
+
+s = Sony('PlayStation')
+
+print(s.project)
+
+s.project = 'SWQA'
+
+print(s.project)
+
+del s.project
+-------------------------------------------------------------
